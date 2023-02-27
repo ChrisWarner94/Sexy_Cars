f1672254ed666433e2de53b0af2432063e4b450a
Christopher Warner <c.warner1994@hotmail.co.uk>
2023-02-27 17:01:45 +0000

    Bookings controller near completion

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,30 +1,42 @@
 class BookingsController < ApplicationController
 
   def index
-
+    @booking = Booking.all
   end
 
   def show
-
+    @booking = @booking.find(params[:id])
   end
 
   def new
-
+    @booking = Booking.new
   end
 
   def create
-
+    @booking = Booking.new(booking_params)
+    @booking.save
   end
 
   def edit
-
+    @task = Task.find(params[:id])
+    # May not be suitable due to update on card plans
   end
 
   def update
-
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to # booking card if needed
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @Booking.destroy
+    # redirect_to status: :see_other
+  end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user, :car)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
 
-  
+  validates_presence_of :user
+  validates_presence_of :car
 end
