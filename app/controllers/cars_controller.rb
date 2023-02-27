2811a6da8cc9474e323bee1beebac79ae8b43eb4
Alia Eshaq <alia@greenbeeinsights.com>
2023-02-27 17:08:27 +0000

    controller updated

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,28 +1,36 @@
 class CarsController < ApplicationController
-  def index
 
+  def index
+    @cars = Car.all
   end
 
   def show
-
+    @cars = Car.find(params[:id])
   end
 
   def new
-
+    @car = Car.new
   end
 
   def create
-
+    @car = Car.new(car_params)
+    @car.save
+    redirect_to car_path(@car)
   end
 
   def edit
-
   end
-   def edit
 
-   end
 
    def update
-    
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path(@car)
+   end
+
+   private
+
+   def car_params
+    params.require(:car).permit(:model, :number_of_seats, :fuel_type, :colour, :photo)
    end
 end
---
+++ db/migrate/20230227165356_add_photo_to_car.rb
@@ -0,0 +1,3 @@
+class AddPhotoToCar < ActiveRecord::Migration[7.0]
+  def change
+end
