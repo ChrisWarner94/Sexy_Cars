e7fd09566f0544204bb1faa26ed3a79a6dee7cde
simon harwood <sjwharwood@outlook.com>
2023-02-28 16:56:31 +0000

    cloudinary connected, photo attached to faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,41 +6,41 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :cars
 
-  # FIRST_NAME & LAST_NAME VALIDATES PRESESNCE TRUE
+#   # FIRST_NAME & LAST_NAME VALIDATES PRESESNCE TRUE
 
-  validates :first_name, :last_name, presence: :true
+#   validates :first_name, :last_name, presence: :true
 
-  #VALIDATES EMAIL
+#   #VALIDATES EMAIL
 
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+#   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  # PASSWORD VALIDATION
+#   # PASSWORD VALIDATION
 
-  PASSWORD_FORMAT = /\A
-  (?=.{8,})          # Must contain 8 or more characters
-  (?=.*\d)           # Must contain a digit
-  (?=.*[a-z])        # Must contain a lower case character
-  (?=.*[A-Z])        # Must contain an upper case character
-  (?=.*[[:^alnum:]]) # Must contain a symbol
-/x
+#   PASSWORD_FORMAT = /\A
+#   (?=.{8,})          # Must contain 8 or more characters
+#   (?=.*\d)           # Must contain a digit
+#   (?=.*[a-z])        # Must contain a lower case character
+#   (?=.*[A-Z])        # Must contain an upper case character
+#   (?=.*[[:^alnum:]]) # Must contain a symbol
+# /x
 
-  validates :password,
-  presence: true,
-  length: { in: Devise.password_length },
-  format: { with: PASSWORD_FORMAT },
-  confirmation: true,
-  on: :create
+#   validates :password,
+#   presence: true,
+#   length: { in: Devise.password_length },
+#   format: { with: PASSWORD_FORMAT },
+#   confirmation: true,
+#   on: :create
 
-  validates :password,
-  allow_nil: true,
-  length: { in: Devise.password_length },
-  format: { with: PASSWORD_FORMAT },
-  confirmation: true,
-  on: :update
+#   validates :password,
+#   allow_nil: true,
+#   length: { in: Devise.password_length },
+#   format: { with: PASSWORD_FORMAT },
+#   confirmation: true,
+#   on: :update
 
-  # PHONE NUMBER VALIDATION
+#   # PHONE NUMBER VALIDATION
 
-  validates :phone,:presence => true,
-   :numericality => true,
-    :length => { :minimum => 10, :maximum => 15 }
+#   validates :phone,:presence => true,
+#    :numericality => true,
+#     :length => { :minimum => 10, :maximum => 15 }
 end
