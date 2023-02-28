78be6b4766cbb87373355f913ba4e9b30d044425
simon harwood <sjwharwood@outlook.com>
2023-02-28 15:12:06 +0000

    seed.rb completed & DB populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,22 @@
 require 'faker'
 
+
+user = User.new(
+  first_name: "Chris",
+  last_name: "Warner",
+  email: "c.warner15@gmail.com",
+  password: "password123"
+)
+user.save!
+
 100.times do
-  vehicles = Car.new(
+  vehicle = Car.new(
     number_of_seats: ["2", "5", "7"].sample,
     model: Faker::Vehicle.make_and_model,
     fuel_type: Faker::Vehicle.fuel_type,
-    colour: Faker::Vehicle.color
+    colour: Faker::Vehicle.color,
+    user_id: user.id
   )
-  vehicles.save!
+  vehicle.save!
+
 end
-
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
