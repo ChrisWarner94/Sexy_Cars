214c4ae9acb885432872d01c14f9df2be5d3b396
Alia <122747438+aliaeshaq889@users.noreply.github.com>
2023-03-01 15:46:34 +0000

    Update 20230301135741_add_columns_to_bookings.rb

--- db/migrate/20230301135741_add_columns_to_bookings.rb
+++ db/migrate/20230301135741_add_columns_to_bookings.rb
@@ -5,8 +5,3 @@ class AddColumnsToBookings < ActiveRecord::Migration[7.0]
   end
 end
 
-class AddRatingToRestaurants < ActiveRecord::Migration[7.0]
-  def change
-    add_column :restaurants, :rating, :integer, default: 0, null: false
-  end
-end
