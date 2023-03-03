9f657a346f175ae88015964f3bded3629aab7d79
Aisha Hersi <aishahersi509@gmail.com>
2023-03-03 12:31:33 +0000

    added latitude longitude address price to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_135741) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_104535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_135741) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
+    t.integer "price"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
